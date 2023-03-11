56d9d17a3e16b7f5e8a1a7742be40c3095d6c4bb
Oleg_Kolomin <89177999655i@gmail.com>
2023-03-11 20:55:00 +0500

    zadacha19

--- zadacha 19/Program.cs
+++ zadacha 19/Program.cs
@@ -7,3 +7,27 @@
 // 12821 -> да
 
 // 23432 -> да
+
+Console.Write("Ведите первое число: ");
+int namberX = Convert.ToInt32(Console.ReadLine());
+int namber = 0;
+int namber1 =0;
+int namber2 =0;
+int namber3 =0;
+
+namber = namberX/10000;
+namber1 = namberX/1000;
+namber1 = namber1%10; 
+namber2 = namberX%100;
+namber2 = namber2/10;
+namber3 = namberX %10;
+
+
+if(namber1 == namber2 && namber == namber3)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
